e8f8ae7a6464472efcbbf568a803d2bfd36755a3
Dmitry <pro100pro10010@gmail.com>
2023-12-15 18:30:19 +0300

    Update [setup_precommit] global

--- setup_precommit.sh
+++ setup_precommit.sh
@@ -3,6 +3,6 @@ echo "#!/bin/sh
 pipenv run pip freeze > requirements.txt
 pipenv run ruff format .
 pipenv run ruff check . --fix --exit-non-zero-on-fix
-FILES=\`git diff --name-only --cached\`
+FILES=\$(git diff --diff-filter=d --name-only)
 git add \$FILES requirements.txt" > .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
